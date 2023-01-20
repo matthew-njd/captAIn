4cbde323c9083d3f30da467417946cc363fcee3b
mattyydagz <mnjdagostino@gmail.com>
2023-01-20 14:00:13 -0500

    Finished script and added new user image

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 import bot from "./assets/bot.svg";
-import user from "./assets/user.svg";
+import user from "./assets/user.png";
 
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
@@ -42,12 +42,43 @@ function generateUniqueId() {
 function chatStripe(isAi, value, uniqueId) {
   return `
       <div class="wrapper ${isAi && "ai"}">
-        <div class="chat">
-          <div>
-
+          <div class="chat">
+              <div class="profile">
+                  <img 
+                    src=${isAi ? bot : user} 
+                    alt="${isAi ? "bot" : "user"}" 
+                  />
+              </div>
+              <div class="message" id=${uniqueId}>${value}</div>
           </div>
-        </div>
       </div>
-
-    `;
+  `;
 }
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  //user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset();
+
+  //bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, "", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
